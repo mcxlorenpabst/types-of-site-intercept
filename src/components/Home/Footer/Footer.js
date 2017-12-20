6412eaa06e1d9fd024c8da67e7517387099870a9
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-20 14:07:45 -0700

    added footer to home page

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -19,7 +19,40 @@ class Footer extends Component {
   render() {
     return (
       <section className='footer'>
-        
+        <div className='footer_inner'>
+          <div className='footer_about'>
+            MaritzCX combines technology, data, and research expertise to make sure the right people in your organization can understand and respond to every customer experience in real time.
+          </div>
+
+          <div className='footer_menu'>
+            <ul>
+              <li>What is CX?</li>
+              <li>Voice of the Customer (VoC)</li>
+              <li>Survey Program Solutions</li>
+              <li>Customer Journey Mapping</li>
+              <li>Net Promoter Score (NPS)</li>
+              <li>CX Key Terms</li>
+            </ul>
+            <ul>
+              <li>Learn</li>
+              <li>CX Café</li>
+              <li>Product Training</li>
+              <li>Events</li>
+              <li>Press Releases</li>
+            </ul>
+            <ul>
+              <li>Contact Us</li>
+              <li>About</li>
+              <li>Careers</li>
+              <li>Our Story</li>
+              <li>Leave Feedback</li>
+            </ul>
+          </div>
+
+          <div className='footer_base'>
+
+          </div>
+        </div>
       </section>
     );
   }
