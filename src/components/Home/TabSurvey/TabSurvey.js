f32380fa6a7c926e8dc798d1348ab250a7ffa616
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-21 12:12:10 -0700

    finished tab survey design and functinality

--- src/components/Home/TabSurvey/TabSurvey.js
+++ src/components/Home/TabSurvey/TabSurvey.js
@@ -7,10 +7,12 @@ class TabSurvey extends Component {
     super(props);
 
     this.state = {
-      hideTabSurvey: true
+      hideTabSurvey: true,
+      destroyTabSurvey: false,
     }
 
     this.toggleTabSurvey = this.toggleTabSurvey.bind(this);
+    this.destroyTabSurvey = this.destroyTabSurvey.bind(this);
   }
 
   toggleTabSurvey(){
@@ -19,12 +21,25 @@ class TabSurvey extends Component {
     })
   }
 
+  destroyTabSurvey(){
+    this.setState({
+      destroyTabSurvey: true
+    })
+  }
+
   render() {
     let right = this.state.hideTabSurvey ? -800 : 0;
     return (
-      <section className='tab_survey' style={{right: right + 'px'}}>
-        <div className='feedback_tab' onClick={this.toggleTabSurvey} >Feedback</div>
-        <iframe src='https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?id=SXWD5F' title='MaritzCX Survey' className='tab_survey_frame' id='tabSurveyFrame' />
+      <section>
+        {
+          this.state.destroyTabSurvey ? 
+            null
+          : <div className='tab_survey' style={{right: right + 'px'}}>
+              <div className='feedback_tab' onClick={this.toggleTabSurvey} >Feedback</div>
+              <div className='tab_survey_close' onClick={this.destroyTabSurvey} >X</div>
+              <iframe src='https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?idx=SXWD5F' title='MaritzCX Survey' className='tab_survey_frame' id='tabSurveyFrame' />
+            </div>
+        }
       </section>
     );
   }
