044a5f2ed1ba06bb43b630696292ca7b0ef9c0de
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-21 09:06:25 -0700

    made additional changes to bottom survey

--- src/components/Home/BottomSurvey/BottomSurvey.js
+++ src/components/Home/BottomSurvey/BottomSurvey.js
@@ -3,33 +3,39 @@ import './BottomSurvey.css';
 
 class BottomSurvey extends Component {
 
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
+        this.state = {
+            showIframe: true
+        }
 
     }
 
-  }
+    componentDidMount() {
+        window.addEventListener("message", receiveMessage, false);
+        
+        function receiveMessage(event){
+          if (event.origin !== "https://alias.allegiancetech.com")
+            return;
+            console.log(event);
+        }
+        let frame = document.getElementById('surveyFrame');
+        frame.contentWindow.postMessage('hello', 'http://localhost:3000')
+    }
 
-  componentDidMount(){
-    
-  }
+    render() {
+            let frameHeight = this.state.showIframe ? 800 : 0;
 
-  render() {
-    return (
-      <section className='bottom_survey'>
-        <div className='bottom_survey_inner'>
-            <p>Did you find what you were looking for?</p>
-            <div className='bottom_survey_btns'>
-                <div className='btn yes_btn'>Yes</div>
-                <div className='btn no_btn'>No</div>
-            </div>
-        </div>
-      </section>
-    );
-  }
-}
+            return (
+                <section className='bottom_survey'>
+                    <div className='bottom_survey_inner'>
+                        <iframe src='https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?idx=SXWD5F' className='bottom_survey_frame' height={frameHeight} title='surveyFrame' id='surveyFrame' />     
+                    </div>
+                </section>
+            );
+        }
+    }
 
 
-export default BottomSurvey;
+    export default BottomSurvey;
