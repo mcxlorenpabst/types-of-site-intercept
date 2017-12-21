d8fcf26d53991a9cdf64f6c6b407f32c07832e4f
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-21 12:22:23 -0700

    adjusted bottom survey to have a close button

--- src/components/Home/BottomSurvey/BottomSurvey.js
+++ src/components/Home/BottomSurvey/BottomSurvey.js
@@ -7,35 +7,43 @@ class BottomSurvey extends Component {
         super(props);
 
         this.state = {
-            showIframe: true
+            showIframe: true,
+            destroyBottomSurvey: false,
         }
 
+        this.destroyBottomSurvey = this.destroyBottomSurvey.bind(this);
     }
 
-    componentDidMount() {
-        window.addEventListener("message", receiveMessage, false);
-        
-        function receiveMessage(event){
-          if (event.origin !== "https://alias.allegiancetech.com")
-            return;
-            console.log(event);
-        }
-        let frame = document.getElementById('surveyFrame');
-        frame.contentWindow.postMessage('hello', 'http://localhost:3000')
+    destroyBottomSurvey(){
+        this.setState({
+            showIframe: false
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    destroyBottomSurvey: true
+                })
+            }, 500)
+        })
     }
 
     render() {
-            let frameHeight = this.state.showIframe ? 800 : 0;
+        let frameHeight = this.state.showIframe ? 800 : 0;
 
-            return (
-                <section className='bottom_survey'>
-                    <div className='bottom_survey_inner'>
-                        <iframe src='https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?idx=SXWD5F' className='bottom_survey_frame' height={frameHeight} title='surveyFrame' id='surveyFrame' />     
-                    </div>
-                </section>
-            );
-        }
+        return (
+            <section>
+                {
+                    this.state.destroyBottomSurvey ? 
+                        null
+                    :   <div className='bottom_survey' style={{height: frameHeight + 'px'}}>
+                            <div className='bottom_survey_inner'>
+                                <div className='tab_survey_close' onClick={this.destroyBottomSurvey} >X</div>
+                                <iframe src='https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?idx=SXWD5F' className='bottom_survey_frame' style={{height: frameHeight + 'px'}} title='surveyFrame' id='surveyFrame' />     
+                            </div>
+                        </div>
+                }
+            </section>
+        );
     }
+}
 
-
-    export default BottomSurvey;
+export default BottomSurvey;
--- src/router.js
+++ src/router.js
@@ -9,7 +9,6 @@ export default (
     <Switch>
         
         <Route component={ Home } path='/' exact />
-        <Route component={ Home } path='/:type' exact />
 
     </Switch>
 )
