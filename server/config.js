2375f73ee30f3c81d4608d620db54745306a3060
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-01-10 14:52:19 -0700

    removed config from gitignore

---
+++ server/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    secret: "aetbmjljfcbnaq4343ghnanmafih2il3h4;kljq;l34345nlk",
+    connection: {
+        host: '',
+            port: '',
+        database: '',
+            user: '',
+        password: '',
+            ssl: true
+    },
+    port: 8000
+};
